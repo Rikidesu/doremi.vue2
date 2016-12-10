07271eda25ff2d9bf8a8d9ed90de5042bce8be4e
Rikiponzu* <l@riki.cc>
2016-12-10 20:01:19 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
     <div id="side" class="side">
-        <div class="progress">
-            <i class="fa fa-music"></i>
+        <div class="progress" :style="{transform:'rotate('+(audio.currentTime/audio.duration)*360+'deg)'}">
+            <i :style="{transform:'rotate(-'+(audio.currentTime/audio.duration)*360+'deg)'}" class="fa fa-music"></i>
+            {{audio.currentTime}}
+            {{audio.duration}}
         </div>
         <!-- 这里做成3D的， 反面可以翻过来，进行下载、评星等操作-->
         <div class="cover">
 
-            <img width="100%" src="dist/img/cover.png">
+            <img width="100%" :src="coverUrl">
 
             <div class="controller">
 
@@ -15,7 +17,7 @@
 
                 </div>
 
-                <div @click="play()" class="play">
+                <div @click="isPlay?pause():play()" class="play">
                     <i class="fa" :class="{'fa-pause':isPlay,'fa-play':!isPlay}"></i>
                 </div>
                 <div class="time">
@@ -35,15 +37,25 @@
 
     </div>
 
-    <div class="main">
+    <div class="lrcboard" :class="{blur:isSearch}" id="lrcboard">
+        <ul id="lrc">
+            <li style=" line-height: 1000%;">♪~ ゛(´д｀*)゛~♪~♪　ﾌﾝﾌﾝ</li>
+        </ul>
+    </div>
 
-        <div class="header">
-            <h1> doremi.moe </h1>
+    <div class="searchBoard" :class="{active:isSearch}">
+
+      <div v-if="showSearch||searchInput||searchResults" @click="closeSearch" style="position:fixed;z-index:2;width:100px;height:80px;line-height:80px;text-align:center;background-color:rgba(0,0,0,.6);font-size:60px;color:#fff;cursor:pointer">
+        <i class="fa fa-arrow-left"></i>
+      </div>
+
+        <div class="header" :class="{'active':isSearch}">
+            <h2 class="domain"> doremi.moe </h2>
         </div>
-        <div class="search">
+        <div class="search" :class="{'active':isSearch}">
 
-            <form id="search" onsubmit="event.preventDefault();return search.text();">
-                <input name="text" class="searchInput" id="searchInput" placeholder="这里搜索噢～">
+            <form id="search" @submit.prevent="doSearch">
+                <input @focus="showSearch=true" @blur="showSearch=false" name="text" v-model="searchInput" class="searchInput" id="searchInput" placeholder="这里搜索噢～">
                 <label for="searchInput"></label>
             </form>
 
@@ -52,9 +64,39 @@
         </div>
 
         <div id="searchResult" class="body">
+          <transition-group name="searchResultAnimation">
+          <div v-if="searchResults" v-for="(x,$index) in searchSongs" :key="'num_'+$index" class="result searchResultAnimation" :style="{transition:'all '+($index+1)*0.2+'s'}">
+            <div class="coverImg">
+              <img width="100%" :src="x.album.picUrl">
+            </div>
+            <div class="resultTitle">
+              <h3>{{x.name}}</h3>
+                            <span class="ctrl">
+                              <span>
+                                <i class="fa fa-heart-o"></i>
+                              </span>
+                              <span>
+                                <i class="fa fa-star-o"></i>
+                              </span>
+                              <span>
+                                <i @click="play(x.mp3Url)" class="fa fa-play"></i>
+                              </span>
+                            </span>
+            </div>
+            <div class="resultDetail">
+              <span class="singer">
+                <span>歌手：</span><span style="padding-right:5px" v-for="s in x.artists">{{s.name}}</span>
+              </span>
+              <span class="album" :title="x.album.name">
+                专辑：album{{x.album.name}}
+              </span>
 
+
+            </div>
+
+          </div>
+        </transition-group>
         </div>
-
     </div>
   </div>
 </template>
@@ -62,58 +104,88 @@
 <script>
 export default {
   name: 'app',
-  data () {
+  data:function () {
     return {
       audio:document.createElement("audio"),
+      showSearch:false,
+      searchResults:'',
       isPlay:false,
+      currentTime:'',
+      searchSongs:{},
+      searchInput:"",
       autoplay:false,
-      mp3Url:"demo.mp3",
+      mp3Url:"./src/demo.mp3",
+      radio: '1',
+      coverUrl:"./src/img/cover.png",
+      items: [1,2,3,4,5,6,7,8,9]
     }
   },
-  methods () {
-    return {
-      play:function(){
-        if(this.audio.paused){
-          if(!this.audio.src){
-            this.audio.src="demo.mp3";
-          }
+  ready:function(){
+    this.audio.addEventListener("timeupdate",this.updateLrc);
+  },
+  computed:{
+    isSearch:function(){
+      return this.showSearch||this.searchInput||this.searchResults;
+    }
+  },
+  methods:{
+
+    updateLrc:function(){
+      this.currentTime=this.audio.currentTime;
+      console.log(this.currentTime);
+
+    },
+    closeSearch:function(){
+      this.showSearch=false;
+      this.searchInput="";
+      this.searchResults='';
+    },
+      play:function(url){
+        if(url){
+            this.audio.src=url;
+        }
           this.audio.play();
           this.isPlay=true;
-        }else{
+      },
+      pause:function(){
           this.audio.pause();
           this.isPlay=false;
-        }
+      },
+      doSearch:function(){
+        this.$http.get('http://127.0.0.1/api/search/'+this.searchInput+"/0/10",{})
+        .then(function(res){
+          this.searchResults=res.data;
+            this.searchSongs=res.data.result.songs;
+        },function(res){
+          console("error");
+        })
+
       }
-    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#app{
+  height:100%;
+  overflow-y: auto;
+}
+.searchResultAnimation{
+  opacity:1;
+}
+.searchResultAnimation-enter{
+  transform: scale(.8) rotateY(90deg);
+  opacity:0;
+}
+.searchResultAnimation-enter-active{
+}
+.searchResultAnimation-leave{
+  opacity:0;
 }
 
-h1, h2 {
-  font-weight: normal;
+.searchResultAnimation-leave-active{
+  transition: all .3s!important;
+  opacity:0;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-import fontawesome from 'font-awesome-webpack'
-import App from './App.vue'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+require("./assets/font-awesome/css/font-awesome.min.css");
+require("./default.css");
+import App from './App.vue';
+import vueResource from 'vue-resource';
 
+Vue.use(vueResource);
+
+Vue.use(ElementUI);
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
 
-Vue.use(ElementUI)
+
+//Vue.use(fontAwesome);
