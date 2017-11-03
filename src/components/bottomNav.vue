f91655aafce54f5eab95826a23549877444a2bcd
Rikiponzu* <l@riki.li>
2017-11-03 10:01:33 +0800

    add vue-router to project

---
+++ src/components/bottomNav.vue
@@ -0,0 +1,151 @@
+<template>
+
+    <div id="menu" class="menu">
+
+        <div class="menu-tip">
+            menu
+        </div>
+        <div class="menu-content">
+            <ul class="menu-nav" ref="menu">
+                <li class="menu-nav-li">
+                    <div class="menu-nav-li-bg">
+                        <router-link to="/">首页</router-link>
+                    </div>
+                </li>
+                <li class="menu-nav-li">
+                    <div class="menu-nav-li-bg">
+                        <router-link to="/foo">音乐库</router-link>
+                    </div>
+                </li>
+                <li class="menu-nav-li">
+                    <div class="menu-nav-li-bg">
+                        <router-link to="/bar">我喜欢的</router-link>
+                    </div>
+                    <!--  -->
+                </li>
+                <li class="menu-nav-li">
+                    <div class="menu-nav-li-bg">我的收藏</div>
+                    <!-- <router-link to="/bar">Go to Bar</router-link> -->
+                </li>
+                <li class="menu-nav-li">
+                    <div class="menu-nav-li-bg">其他</div>
+                    <!-- <router-link to="/bar">Go to Bar</router-link> -->
+                </li>
+                <li class="menu-nav-li">
+                    <div class="menu-nav-li-bg">其他</div>
+                    <!-- <router-link to="/bar">Go to Bar</router-link> -->
+                </li>
+                <li class="menu-nav-li">
+                    <div class="menu-nav-li-bg">其他</div>
+                    <!-- <router-link to="/bar">Go to Bar</router-link> -->
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+</template>
+
+
+<script>
+
+    "use strict";
+    export default {
+
+        // name:"leftnav",
+        //props:['data'],
+        data:()=>{
+            return{
+            }
+        },
+        methods:{
+
+        }
+
+
+    }
+
+
+</script>
+
+
+<style>
+    .menu{
+        position:fixed;
+        bottom:0;
+        left:0;
+        margin-left: 250px;
+        width:calc( 100% - 250px );
+        height:60px;
+        /*padding:60px 0 200px 0;*/
+        z-index:1000;
+        display: flex;
+        box-sizing:border-box;
+        align-items: center;
+    }
+    .menu:hover .menu-content{
+        opacity: 1;
+        pointer-events: auto;
+    }
+    .menu-content{
+        margin-top: -85px;
+        width:100%;
+        overflow-x: auto;
+        overflow-y: hidden;
+        pointer-events: none;
+        opacity: 0;
+        transition-duration: 0.3s;
+    }
+    .menu-nav{
+        /*width:240px;*/
+        white-space: nowrap;
+        list-style:none;
+        transform-origin: left;
+        transition-duration: 0.3s;
+        /*transform: rotateY(90deg);*/
+        transform-style: preserve-3d;
+        transition-timing-function: ease;
+    }
+    .menu:hover .menu-nav{
+        /*transform: rotateY(0deg);*/
+    }
+    .menu:hover .menu-nav-li{
+        opacity: 1;
+        transform: rotateZ(-8deg) translateY(0px);
+    }
+    .menu-nav-li{
+        display: inline-block;
+        padding:20px 60px;
+        font-size: 30px;
+        color: rgba(81,81,81,.8);
+        opacity: 0;
+        transform: rotateZ(0deg) translateY(30px);
+        transition-duration: 0.3s;
+        transition-timing-function: ease;
+        transform-style: preserve-3d;
+        text-shadow: 1px 1px 1px 1px rgba(222,222,222,.3);
+    }
+    .menu-nav-li-bg{
+        border-radius: 50px;
+        padding: 3px 15px;
+        background-color: rgba(255,255,255,.9);
+        box-shadow: 0px 0px 10px 9px rgba(255,255,255,.9);
+    }
+    .menu-tip{
+        position:absolute;
+        bottom:-1px;
+        left:calc( 50% - 130px );
+        border:solid 1px rgba(66,66,66,.4);
+        padding:1px 4px;
+        border-top-left-radius: 3px;
+        border-top-right-radius: 3px;
+        background-color:rgba(233,233,233,.5);
+        transition:all .4s;
+        transition-timing-function: ease;
+        opacity: 1;
+    }
+    .menu:hover .menu-tip{
+        bottom:-5px;
+        opacity: 0;
+    }
+</style>
--- src/components/lrcBoard.vue
+++ src/components/lrcBoard.vue
@@ -1,5 +1,5 @@
 <template>
-        <div class="lrcboard" :class="{blur:isSearch&&!lrc.alwaysShow,zoom:lrc.alwaysShow&&config.secondScreen}" id="lrcboard">
+        <div class="lrcboard" :class="{blur:!config.alwaysShow&&(isSearch||routePath!='/'),zoom:config.alwaysShow&&(config.secondScreen||routePath!='/')}" id="lrcboard">
             <ul id="lrc">
                 <li v-if="!lrc.result" style=" line-height: 1000%;">ヽ(*´∀｀*)ノ.+ﾟおはよ～♪.+ﾟ</li>
                 <li :class="{active:$index==lrc.now-1}" v-for="(x,$index) in lrc.result">
@@ -33,6 +33,9 @@
                 return this.$store.state.search
             },
             isSearch: state => state.config.secondScreen = state.search.show||state.search.input||state.search.result,
+            routePath:function(){
+                return this.$route.fullPath
+            }
         }),
 
 
@@ -43,5 +46,84 @@
 
 
 <style>
+
+    .lrcboard{
+        position: absolute;
+        top:0;
+        right:0;
+        width:100%;
+        height:100%;
+        overflow-y: auto;
+        overflow-x: hidden;
+        text-align:center;
+        font-size: 24px;
+        background-color:rgba(255,255,255,.3);
+        text-shadow: 0 0 6px rgba(0,0,0,.4);
+        -webkit-transition-duration: .6s;
+        -moz-transition-duration: .6s;
+        -ms-transition-duration: .6s;
+        -o-transition-duration: .6s;
+        transition-duration: .6s;
+        /*transition-delay: .3s;*/
+    }
+    .lrcboard.zoom{
+        width:40%;
+        font-size:18px;
+        /*border-right:solid 1px #666*/
+    }
+    .lrcboard.blur{
+        -webkit-filter: blur(10px); /* Chrome, Opera */
+        -moz-filter: blur(10px);
+        -ms-filter: blur(10px);
+        filter: blur(10px);
+    }
+    ul#lrc {
+        padding-left:50px;
+        padding-right:50px;
+        -webkit-transform-style: preserve-3d;
+        -moz-transform-style: preserve-3d;
+        -ms-transform-style: preserve-3d;
+        transform-style: preserve-3d;
+        backface-visibility: hidden;
+        list-style: none;
+    }
+    .lrcboard li{
+        margin:32px 0 32px 0;
+        /*white-space:pre-wrap;*/
+        -webkit-transition-duration: .6s;
+        -moz-transition-duration: .6s;
+        -ms-transition-duration: .6s;
+        -o-transition-duration: .6s;
+        transition-duration: .6s;
+    }
+    .lrc {
+        white-space: pre;
+    }
+    .tLrc {
+        position:absolute;
+        left:0;
+        right:0;
+        font-size:14px;
+        color:rgba(64, 77, 91, 0.8)
+    }
+    .lrcboard li.active .tLrc{
+        color:#6698ff;
+    }
+    /*
+    .lrcboard li.hidden{
+        opacity: 0;
+    }
+    */
+    .lrcboard li.active{
+        /*willchange:transform;*/
+        -webkit-transform: scale(1.1);
+        -moz-transform: scale(1.1);
+        -ms-transform: scale(1.1);
+        -o-transform: scale(1.1);
+        transform: scale(1.1);
+        color:#6698ff;
+        text-shadow: 0 0 6px #6698ff;
+    }
+
 
 </style>
