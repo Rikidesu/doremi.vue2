196c70aa3019649546dfdf390385a6b968991dfa
Rikiponzu* <l@riki.li>
2017-11-04 18:27:28 +0800

    build

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const http = require('http')
 const app = express()
 
 // 跨域设置
-app.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true)
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "X-Requested-With")
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
-  res.header("X-Powered-By", ' 3.2.1')
-  res.header("Content-Type", "application/json;charset=utf-8")
-  next()
-})
+// app.all('*', function (req, res, next) {
+//   res.header("Access-Control-Allow-Credentials", true)
+//   res.header("Access-Control-Allow-Origin", "*")
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With")
+//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS")
+//   res.header("X-Powered-By", ' 3.2.1')
+//   res.header("Content-Type", "application/json;charset=utf-8")
+//   next()
+// })
 
 process.on('uncaughtException', function(err) {
     console.log(err.stack);
--- src/store/modules/config.js
+++ src/store/modules/config.js
@@ -2,11 +2,11 @@
 * @Author: Rikiponzu*
 * @Date:   2017-10-27 14:34:58
 * @Last Modified by:   Rikiponzu*
-* @Last Modified time: 2017-11-04 14:58:24
+* @Last Modified time: 2017-11-04 18:24:35
 */
 export default {
     state:{
-        host:{ local:"" , doremi:"http://doremi.moe" , dev:"http://127.0.0.1:3000"}.dev,
+        host:{ local:"" , doremi:"http://doremi.moe" , dev:"http://127.0.0.1:3000"}.local,
         secondScreen:false,
         items: [1,2,3,4,5,6,7,8,9],
         updateDelay:500,
